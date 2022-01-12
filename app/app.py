66dde90057c161fa8346774bfa80fa55ecb5f431
Mathushan Mathiyalagan <mathiyalaganmathushan@gmail.com>
2022-01-12 17:38:30 +0000

    Initial changes

--- app/app.py
+++ app/app.py
@@ -1,13 +1,13 @@
 from flask import Flask, render_template
 from flask_migrate import Migrate
-from models.QuestionModel import db
-from routes.Question_bp import Question_bp
+from models.User import db
+from routes.user_bp import user_bp
 
 app = Flask(__name__)
 app.config.from_object('config')
 db.init_app(app)
 migrate = Migrate(app, db)
-app.register_blueprint(Question_bp)
+app.register_blueprint(user_bp)
 
 
 @app.route('/')
