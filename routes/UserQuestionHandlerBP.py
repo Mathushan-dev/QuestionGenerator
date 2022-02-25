de134e6c7cd0668616e387a49a876e2b98623b57
Mathushan Mathiyalagan <mathiyalaganmathushan@gmail.com>
2022-02-25 20:09:39 +0000

    removing all files

--- routes/UserQuestionHandlerBP.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint
-from controllers.UserQuestionHandlerController import generateTFQuestions, generateMCQuestions
-
-UserQuestionHandlerBP = Blueprint('UserQuestionHandlerBP', __name__)
-
-UserQuestionHandlerBP.route('/generateTFQuestions', methods=['POST', 'GET'])(generateTFQuestions)
-UserQuestionHandlerBP.route('/generateMCQuestions', methods=['POST', 'GET'])(generateMCQuestions)
