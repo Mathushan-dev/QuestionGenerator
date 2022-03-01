b48d504bf6690ffbe28c40d82678fea0b7c3e855
Orhun Dogan <79797340+orhundogan@users.noreply.github.com>
2022-03-01 15:56:46 +0000

    Add files via upload

---
+++ static/javascript/show-text.js
@@ -0,0 +1,19 @@
+var modal = document.getElementById("text-modal");
+
+var btn = document.getElementById("show-text-button");
+
+var span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
