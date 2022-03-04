b6466fc9d40f589d4e738a414fcdc0a0dfe00ac2
Mathushan Mathiyalagan <mathiyalaganmathushan@gmail.com>
2022-03-04 17:43:28 +0000

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ SECRET_KEY = os.urandom(32)
 # Grabs the folder where the script runs.
 basedir = os.path.abspath(os.path.dirname(__file__))
 # Enable debug mode.
-DEBUG = True
+DEBUG = False
 # Connect to the database
 SQLALCHEMY_DATABASE_URI = 'postgresql://vczoqebk:Xyf9h4SyYkWYyeSYu3N9oE4wqIloAYI7@jelani.db.elephantsql.com/vczoqebk'
 # Turn off the Flask-SQLAlchemy event system and warning
