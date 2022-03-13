8e66f8c12fed9c57b6f513cc89642de79e3f8a77
Mathushan Mathiyalagan <mathiyalaganmathushan@gmail.com>
2022-03-13 19:13:27 +0000

    Update test_UserAPIController.py

--- tests/test_UserAPIController.py
+++ tests/test_UserAPIController.py
@@ -3,21 +3,41 @@ import requests
 
 def test_generate_mc_question_api():
     try:
-        response = requests.post('http://192.168.169.77:5000/generateMCQuestionAPI?context="Jack walked to the '
+        response = requests.post('http://40.87.57.232:5000/generateMCQuestionAPI?context="Jack walked to the '
                                  'shop"&numberOptions=4')
         assert (response.status_code == 200)
         response.raise_for_status()
         # Code here will only run if the request is successful
-    except:
-        print("Error in test_generate_mc_question_api")
+    except requests.exceptions.HTTPError as errh:
+        print(errh)
+        assert False
+    except requests.exceptions.ConnectionError as errc:
+        print(errc)
+        assert False
+    except requests.exceptions.Timeout as errt:
+        print(errt)
+        assert False
+    except requests.exceptions.RequestException as err:
+        print(err)
+        assert False
 
 
 def test_generate_tf_question_api():
     try:
         response = requests.post(
-            'http://192.168.169.77:5000/generateTFQuestionAPI?context="Jack walked to the shop"')
+            'http://40.87.57.232:5000/generateTFQuestionAPI?context="Jack walked to the shop"')
         assert (response.status_code == 200)
         response.raise_for_status()
         # Code here will only run if the request is successful
-    except:
-        print("Error in test_generate_tf_question_api")
+    except requests.exceptions.HTTPError as errh:
+        print(errh)
+        assert False
+    except requests.exceptions.ConnectionError as errc:
+        print(errc)
+        assert False
+    except requests.exceptions.Timeout as errt:
+        print(errt)
+        assert False
+    except requests.exceptions.RequestException as err:
+        print(err)
+        assert False
