2cdfe7f310c3f4cb7c035bcccd425a2a69b00215
Mathushan Mathiyalagan <mathiyalaganmathushan@gmail.com>
2022-03-19 11:24:34 +0000

    mutmut mutation testing

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ class Config:
     SQLALCHEMY_DATABASE_URI = 'postgresql://vczoqebk:Xyf9h4SyYkWYyeSYu3N9oE4wqIloAYI7@jelani.db.elephantsql.com/vczoqebk'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     FLASK_ENV = 'production'
-    DEBUG = True
-    TEST = True
+    DEBUG = False
+    TEST = False
     STATIC_FOLDER = 'static'
     TEMPLATES_FOLDER = 'templates'
