9012ae610cab45bd978ab2372bb00687bfbd54bf
Mathushan Mathiyalagan <mathiyalaganmathushan@gmail.com>
2022-03-23 19:10:41 +0000

    test ci check

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ class Config:
     SQLALCHEMY_DATABASE_URI = 'postgresql://vczoqebk:Xyf9h4SyYkWYyeSYu3N9oE4wqIloAYI7@jelani.db.elephantsql.com/vczoqebk'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     FLASK_ENV = 'production'
-    DEBUG = False
-    TEST = False
+    DEBUG = True
+    TEST = True
     STATIC_FOLDER = 'static'
     TEMPLATES_FOLDER = 'templates'
